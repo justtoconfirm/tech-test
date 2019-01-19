5aa3f943fba821bbca1873a30328ec8b5cc2aa31
justtoconfirm <office@justtoconfirm.co.uk>
2019-01-19 14:47:12 +0000

    Initial React component added to make AJAX call using the Fetch API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
+import MyComponent from './components/MyComponent'
 
 render (
-	<h1>Hello, World</h1>,
+	<MyComponent />,
 	document.getElementById('app')
 )
